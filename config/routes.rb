10097821340146b4f933bd47ddd49a4a6033d191
NicMortelliti <npmortelliti@gmail.com>
2022-10-22 16:00:00 -0700

    Add users resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  
-  resources :vehicles
   resources :flights
   resources :reservations
+  resources :users
+  resources :vehicles
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
