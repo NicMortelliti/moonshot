803494970a5d23fc59d7039e427d49b3bdbf94b6
NicMortelliti <npmortelliti@gmail.com>
2022-10-24 15:51:37 -0700

    Add create method

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,3 +1,15 @@
 class VehiclesController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  # POST '/vehicles'
+  def create
+    vehicle = Vehicle.create!(vehicle_params)
+    render json: vehicle, status: :created
+  end
+
+  private
+
+  def vehicle_params
+    params.permit(:make, :model, :name, :pax_capacity, :image)
+  end
 end
