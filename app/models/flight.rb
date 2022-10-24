33796e2980f0be596b365a3e06ada2d2ce7f1ab0
NicMortelliti <npmortelliti@gmail.com>
2022-10-24 13:47:30 -0700

    Add validations to flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,12 @@
 class Flight < ApplicationRecord
+  validates :vehicle_id, present
+  validates :origin, present, length: { in: 2..254 }
+  validates :destination, present, length: { in: 2..254 }
+  validates :origin_image, present
+  validates :destination_image, present
+  validates :departure, present
+  validates :arrival, present
+
   has_many :reservations
   has_many :users, through: :reservations
   belongs_to :vehicle
