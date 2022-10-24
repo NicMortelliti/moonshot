b0661343997a0a5551d3c9fd77edf6e7ed2c92b0
NicMortelliti <npmortelliti@gmail.com>
2022-10-24 15:30:06 -0700

    Add destroy method

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,17 @@
 class ReservationsController < ApplicationController
-
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
- 
+
   # POST /reservations
   def create
     reservation = @current_user.reservation.create!(reservation_params)
     render json: reservation
+  end
+
+  # DESTROY '/reservations/[:id]'
+  def destroy
+    reservation = find_reservation
+    reservation.destroy
+    head :no_content
   end
 
   private
@@ -13,4 +19,9 @@ class ReservationsController < ApplicationController
   def appointment_params
     params.permit(:user_id, :flight_id)
   end
+
+  def find_reservation
+    @current_user.reservation.find(params[:id])
+  end
+
 end
