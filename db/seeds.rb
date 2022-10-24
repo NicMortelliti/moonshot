7cf748a922a020f4c3f8d522bf0a73bb1d854e44
NicMortelliti <npmortelliti@gmail.com>
2022-10-24 14:39:31 -0700

    Add vehicle to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Vehicle.create({make: "Jetson", model: "S380", name: "Astro", pax_capacity: 12,  image: "placeholder"})
