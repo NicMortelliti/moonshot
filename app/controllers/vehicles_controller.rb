6614d662da6285496dc375e9d91e5068d2dd4385
NicMortelliti <npmortelliti@gmail.com>
2022-10-24 15:53:20 -0700

    Add destroy method

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -7,9 +7,20 @@ class VehiclesController < ApplicationController
     render json: vehicle, status: :created
   end
 
+  # DESTROY '/vehicles/[:id]
+  def destroy
+    vehicle = find_vehicle
+    vehicle.destroy
+    head :no_content
+  end
+
   private
 
   def vehicle_params
     params.permit(:make, :model, :name, :pax_capacity, :image)
+  end
+
+  def find_vehicle
+    Vehicle.find(params[:id])
   end
 end
