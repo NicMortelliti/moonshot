ed0a6ee6056b50bee2ce9c82408ef7ec08217ce0
NicMortelliti <npmortelliti@gmail.com>
2022-10-24 15:16:15 -0700

    Add user create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  skip_before_action :authorize, only: [:create]
+
+  # POST /users
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages }, status: :unauthorized
+  end
+
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+          .with_defaults(admin: false)
+  end
 end
