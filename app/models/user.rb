490fdc1ca5a7713c04a9cd5a46adfe9ac160f3b8
NicMortelliti <npmortelliti@gmail.com>
2022-10-24 15:10:14 -0700

    Fix validation typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :first_name, present, length: { in: 2..254 }
-  validates :last_name, present, length: { in: 2..254 }
+  validates :first_name, presence: true, length: { in: 2..254 }
+  validates :last_name, presence: true, length: { in: 2..254 }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: { case_sensitive: false }
 
   has_many :reservations
