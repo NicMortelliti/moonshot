8cecbe38636a784698e17d99896e36f995d328c2
NicMortelliti <npmortelliti@gmail.com>
2022-10-24 15:38:24 -0700

    Add create method

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,15 @@
 class FlightsController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-  
+
+  # POST '/flights'
+  def create
+    flight = Flight.create!(flight_params)
+    render json: flight, status: :created
+  end
+
+  private
+
+  def flight_params
+    params.permit(:origin, :destination, :departure, :arrival, :origin_image, :destination_image)
+  end
 end
