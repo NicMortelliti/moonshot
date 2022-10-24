b867e4c8b5d98964211b3d8fb7f498dfdad817c5
NicMortelliti <npmortelliti@gmail.com>
2022-10-24 14:54:38 -0700

    Add authorize method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  before_action :authorize
+
+  private
+
+  def authorize
+    @current_user = Patient.find_by(id: session[:user_id])
+    render json: { errors: ['Not Authorized'] }, status: :unauthorized unless @current_user
+  end
 end
