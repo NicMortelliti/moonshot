196bb51fc40472c4f6b4d82bef7bbbda260fd927
NicMortelliti <npmortelliti@gmail.com>
2022-10-24 15:27:29 -0700

    Add create method

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,4 +2,15 @@ class ReservationsController < ApplicationController
 
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
  
+  # POST /reservations
+  def create
+    reservation = @current_user.reservation.create!(reservation_params)
+    render json: reservation
+  end
+
+  private
+
+  def appointment_params
+    params.permit(:user_id, :flight_id)
+  end
 end
