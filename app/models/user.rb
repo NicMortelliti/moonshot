ecd095f8d50b204734c6361bf91dc56c4e25b944
NicMortelliti <npmortelliti@gmail.com>
2022-10-24 13:51:30 -0700

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :first_name, present, length: { in: 2..254 }
+  validates :last_name, present, length: { in: 2..254 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: { case_sensitive: false }
+
   has_many :reservations
   has_many :flights, through: :reservations
 end
