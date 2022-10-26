8ede183fc94e7218287aff20b1e8f97ad5438c61
NicMortelliti <npmortelliti@gmail.com>
2022-10-26 13:46:03 -0700

    Add function to handle form updates

--- client/src/components/account/EditAccount.js
+++ client/src/components/account/EditAccount.js
@@ -1,4 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+
+function EditAccount({ user }) {
+  const [formData, setFormData] = useState({
+    firstName: user.first_name,
+    lastName: user.last_name,
+  });
+
+  const handleFormFieldChange = (e) =>
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
 
 function EditAccount() {
   return <div>EditAccount</div>;
