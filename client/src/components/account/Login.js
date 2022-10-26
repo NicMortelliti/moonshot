7641071f5c4a3e92a228962c02dca98b457b03a1
NicMortelliti <npmortelliti@gmail.com>
2022-10-26 10:25:05 -0700

    Add function to handle form updates

--- client/src/components/account/Login.js
+++ client/src/components/account/Login.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  // Shared function to handle updating fields
+  const handleFormFieldChange = (e) =>
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
   return <div>Login</div>;
 }
 
