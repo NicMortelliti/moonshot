01fbd0656290bff0509358fafb8df181d3b2f3af
Nic Mortelliti <npmortelliti@gmail.com>
2022-10-25 17:40:21 -0700

    Add handle submit function

--- client/src/components/account/Signup.js
+++ client/src/components/account/Signup.js
@@ -1,9 +1,42 @@
 import React from 'react'
 
 function Signup() {
-  return (
-    <div>Signup</div>
-  )
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    setIsLoading(true);
+
+    fetch("/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        first_name: formData.firstName,
+        last_name: formData.lastName,
+        email: formData.email,
+        password: formData.password,
+        password_confirmation: formData.passwordConfirmation,
+      }),
+    }).then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        r.json().then((user) => onLogin(user));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  };
 }
 
 export default Signup
