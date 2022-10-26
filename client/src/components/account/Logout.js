3dd400f10683281dc03374b750f513782f591f89
NicMortelliti <npmortelliti@gmail.com>
2022-10-26 13:19:58 -0700

    Initial commit for logout component

---
+++ client/src/components/account/Logout.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Logout({ setUser }) {
+  function handleClick() {
+    fetch("/sessions", { method: "DELETE" }).then((r) => {
+      if (r.ok) {
+        setUser(null);
+      }
+    });
+  }
+  return <button onClick={handleClick}>Logout</button>;
+}
+
+export default Logout;
