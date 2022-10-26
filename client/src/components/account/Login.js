858597a9c25887f957f63ebcbef4ca9de3f2e4e1
NicMortelliti <npmortelliti@gmail.com>
2022-10-26 10:26:15 -0700

    Add function to handle form submit

--- client/src/components/account/Login.js
+++ client/src/components/account/Login.js
@@ -1,10 +1,37 @@
 import React, { useState } from "react";
 
-function Login() {
+function Login({ setUser }) {
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    setIsLoading(true);
+
+    fetch("/sessions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: formData.email,
+        password: formData.password,
+      }),
+    }).then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  };
+
   // Shared function to handle updating fields
   const handleFormFieldChange = (e) =>
     setFormData({
