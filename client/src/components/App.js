c3cf80a7a2f5aaf67d430ee55b1a2650471426c4
NicMortelliti <npmortelliti@gmail.com>
2022-10-26 14:46:34 -0700

    Add autologin when page loads

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 // Components
 import { AccountBox } from "./account";
 
 function App() {
   const [user, setUser] = useState(null);
+
+  // auto-login
+  useEffect(() => {
+    fetch("/me").then((r) => {
+      r.ok && r.json().then((user) => setUser(user));
+    });
+  }, []);
 
   return (
     <div className="App">
