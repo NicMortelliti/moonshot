eca3f440a1f42bf7f1b833ec0a377fc23f80ed5c
NicMortelliti <npmortelliti@gmail.com>
2022-10-26 15:37:18 -0700

    Add function to handle form submit

--- client/src/components/body/Search.js
+++ client/src/components/body/Search.js
@@ -1,14 +1,40 @@
 import React, { useState } from "react";
 
-function Search() {
+function Search({ setResults }) {
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [formData, setFormData] = useState({
     origin: "",
     destination: "",
-    roundTrip: "",
+    roundTrip: true,
     depart: "",
-    return: true,
+    return: "",
     numPassengers: 1,
   });
+
+  // Send search params to API
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    setIsLoading(true);
+
+    // Construct url string with search params
+    const url = `/flights
+    ?origin=${formData.origin}
+    &destination=${formData.destination}
+    &departure=${formData.depart}
+    &return=${formData.return}
+    &num_pax=${formData.numPassengers}`;
+
+    fetch(url).then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        r.json().then((results) => setResults(results));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  };
 
   // Update form fields from state
   const handleFormFieldChange = (e) =>
