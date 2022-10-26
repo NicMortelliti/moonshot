959676b078816beb40e8153c788c2b9c372e0347
NicMortelliti <npmortelliti@gmail.com>
2022-10-26 14:57:08 -0700

    Add form data state

--- client/src/components/body/Search.js
+++ client/src/components/body/Search.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Search() {
+  const [formData, setFormData] = useState({
+    origin: "",
+    destination: "",
+    roundTrip: "",
+    depart: "",
+    return: true,
+    numPassengers: 1,
+  });
   return <div>Search</div>;
 }
 
