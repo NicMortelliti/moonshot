9111c2b8a158c645e625ab4b2d3a0016b003e522
NicMortelliti <npmortelliti@gmail.com>
2022-10-27 12:49:01 -0700

    Increase ICAO identifier length

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,6 @@
 class Location < ApplicationRecord
   validates :name, presence: true
-  validates :icao, presence: true, length: { is: 3 }
+  validates :icao, presence: true, length: { is: 4 }
   validates :type, presence: true
 
   has_many :flights
