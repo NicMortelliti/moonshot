0c9981dc9915b8d41f6ae893830127cdba5f1176
NicMortelliti <npmortelliti@gmail.com>
2022-10-27 13:24:21 -0700

    Fix column name

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 class Location < ApplicationRecord
   validates :name, presence: true
   validates :icao, presence: true, length: { is: 4 }
-  validates :type, presence: true
+  validates :macro_place, presence: true
 
   has_many :flights
 end
