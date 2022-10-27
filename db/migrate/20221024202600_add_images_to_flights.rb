bfd0ae58c760e0e10028de1de5fadee239ae5a5f
NicMortelliti <npmortelliti@gmail.com>
2022-10-27 14:57:52 -0700

    Remove images from flight model

--- db/migrate/20221024202600_add_images_to_flights.rb
+++
@@ -1,6 +0,0 @@
-class AddImagesToFlights < ActiveRecord::Migration[6.1]
-  def change
-    add_column :flights, :origin_image, :string
-    add_column :flights, :destination_image, :string
-  end
-end
