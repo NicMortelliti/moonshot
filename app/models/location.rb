1ba09165773d717c3f79d94280abd4ff87971d22
NicMortelliti <npmortelliti@gmail.com>
2022-10-27 12:35:40 -0700

    Add location validations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,7 @@
 class Location < ApplicationRecord
+  validates :name, presence: true
+  validates :icao, presence: true, length: { is: 3 }
+  validates :type, presence: true
+
   has_many :flights
 end
