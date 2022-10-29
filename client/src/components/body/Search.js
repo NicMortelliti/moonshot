ce35a713d1412b5e8cedea2e2a475798097b8858
NicMortelliti <npmortelliti@gmail.com>
2022-10-28 18:22:03 -0700

    Update rendering of search results

--- client/src/components/body/Search.js
+++ client/src/components/body/Search.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-function Search({ setResults }) {
+function Search({ results, setResults }) {
   const [errors, setErrors] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [showSearchBox, setShowSearchBox] = useState(false);
   const [formData, setFormData] = useState({
     origin: "",
     destination: "",
@@ -37,86 +38,134 @@ function Search({ setResults }) {
       }
     });
   };
+  useEffect(() => {
+    console.log(formData);
+  }, [formData, setFormData]);
 
   // Update form fields from state
   const handleFormFieldChange = (e) => {
-    // Set value according to input type
-    const value =
-      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+      // Set value according to input type
+      const value =
+        e.target.type === "checkbox" ? e.target.checked : e.target.value;
+  
+      setFormData({
+        ...formData,
+        [e.target.id]: value,
+      });
+      console.log(e);
+    };
 
-    setFormData({
-      ...formData,
-      [e.target.id]: value,
+  // Display search box and fetch origins from API
+  const handleShowSearch = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    setIsLoading(true);
+
+    fetch("/locations").then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        r.json()
+          .then((data) => setResults(data))
+          .then(() => setShowSearchBox(true));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
     });
   };
 
+  // Display available choices to user
+  const RenderChoicePage = (id) =>
+    results.map((each) => {
+      return (
+        <button
+          key={each.id}
+          id={id}
+          value={each.name}
+          onClick={() => setFormData({ ...formData, origin: each.name })}>
+          <h4>{each.name}</h4>
+          <h5>{each.macro_place}</h5>
+        </button>
+      );
+    });
+
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Origin
-          <input
-            id="origin"
-            type="text"
-            value={formData.origin}
-            onChange={(e) => handleFormFieldChange(e)}
-          />
-        </label>
-        <label>
-          Destination
-          <input
-            id="destination"
-            type="text"
-            value={formData.destination}
-            onChange={(e) => handleFormFieldChange(e)}
-          />
-        </label>
-        <label>
-          Round Trip?
-          <input
-            id="roundTrip"
-            type="checkbox"
-            checked={formData.roundTrip}
-            onChange={(e) => handleFormFieldChange(e)}
-          />
-        </label>
-        <label>
-          Depart
-          <input
-            id="departureDate"
-            type="date"
-            value={formData.departureDate}
-            onChange={(e) => handleFormFieldChange(e)}
-          />
-        </label>
-        {/* Only display return field if roundtrip is true */}
-        {formData.roundTrip && (
-          <label>
-            Return
-            <input
-              id="returnDate"
-              type="date"
-              value={formData.returnDate}
-              onChange={(e) => handleFormFieldChange(e)}
-            />
-          </label>
-        )}
-        <label>
-          Number of Passengers
-          <input
-            id="numPassengers"
-            type="number"
-            value={formData.numPassengers}
-            onChange={(e) => handleFormFieldChange(e)}
-          />
-        </label>
+      {showSearchBox ? (
         <div>
-          <button type="submit">{isLoading ? "Loading..." : "Submit"}</button>
+          <form onSubmit={handleSubmit}>
+            {/* <label>
+              Origin
+              <input
+                id="origin"
+                type="text"
+                value={formData.origin}
+                onChange={(e) => handleFormFieldChange(e)}
+              />
+            </label> */}
+            <RenderChoicePage id="origin" />
+            <label>
+              Destination
+              <input
+                id="destination"
+                type="text"
+                value={formData.destination}
+                onChange={(e) => handleFormFieldChange(e)}
+              />
+            </label>
+            <label>
+              Round Trip?
+              <input
+                id="roundTrip"
+                type="checkbox"
+                checked={formData.roundTrip}
+                onChange={(e) => handleFormFieldChange(e)}
+              />
+            </label>
+            <label>
+              Depart
+              <input
+                id="departureDate"
+                type="date"
+                value={formData.departureDate}
+                onChange={(e) => handleFormFieldChange(e)}
+              />
+            </label>
+            {/* Only display return field if roundtrip is true */}
+            {formData.roundTrip && (
+              <label>
+                Return
+                <input
+                  id="returnDate"
+                  type="date"
+                  value={formData.returnDate}
+                  onChange={(e) => handleFormFieldChange(e)}
+                />
+              </label>
+            )}
+            <label>
+              Number of Passengers
+              <input
+                id="numPassengers"
+                type="number"
+                value={formData.numPassengers}
+                onChange={(e) => handleFormFieldChange(e)}
+              />
+            </label>
+            <div>
+              <button type="submit">
+                {isLoading ? "Loading..." : "Submit"}
+              </button>
+            </div>
+          </form>
+          {errors.map((err) => (
+            <p key={err}>{err}</p>
+          ))}
         </div>
-      </form>
-      {errors.map((err) => (
-        <p key={err}>{err}</p>
-      ))}
+      ) : (
+        <button onClick={(e) => handleShowSearch(e)}>
+          {isLoading ? "Loading..." : "Search"}
+        </button>
+      )}
     </div>
   );
 }
--- client/src/components/body/index.js
+++ client/src/components/body/index.js
@@ -5,12 +5,10 @@ import Search from "./Search";
 
 export function Body(props) {
   const [results, setResults] = useState([]);
+
   return (
     <div>
-      <Search setResults={setResults} />
-      {results.map((each) => (
-        <p key={each.id}>{`${each.origin.name} -> ${each.destination.name}`}</p>
-      ))}
+      <Search results={results} setResults={setResults} />
     </div>
   );
 }
