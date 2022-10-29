b420ac9f1b98b0ada19e885a489019f2467c6fa4
Nic Mortelliti <npmortelliti@gmail.com>
2022-10-29 14:25:50 -0700

    Fix relationships

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,6 +3,9 @@ class Location < ApplicationRecord
   validates :icao, presence: true, length: { is: 4 }
   validates :macro_place, presence: true
 
-  has_many :flights, foreign_key: 'origin'
-  has_many :flights, foreign_key: 'destination'
+  has_many :origins, class_name: 'Flight', foreign_key: 'origin_id'
+  has_many :destinations, class_name: 'Flight', foreign_key: 'destination_id'
+
+  # Set order of objects to ascending by departure date/time
+  default_scope { order(name: :asc) }
 end
