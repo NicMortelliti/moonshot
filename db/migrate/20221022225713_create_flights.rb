3848ff3cff9c86b9ce1493e40eefa56bbd03be2b
NicMortelliti <npmortelliti@gmail.com>
2022-10-28 18:21:44 -0700

    Change id's to integers

--- db/migrate/20221022225713_create_flights.rb
+++ db/migrate/20221022225713_create_flights.rb
@@ -2,8 +2,8 @@ class CreateFlights < ActiveRecord::Migration[6.1]
   def change
     create_table :flights do |t|
       t.string :vehicle_id
-      t.string :origin_id
-      t.string :destination_id
+      t.integer :origin_id
+      t.integer :destination_id
       t.datetime :departure
       t.datetime :arrival
 
