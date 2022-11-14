8f03476e8855baee427bc998aa0adfdff239e6d6
Nic Mortelliti <npmortelliti@gmail.com>
2022-11-14 07:13:21 -0800

    Add commentary to explain approach

--- client/src/components/search/SearchNumber.js
+++ client/src/components/search/SearchNumber.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 function SearchNumber({ min, max, setter }) {
+  // When clicked, set state to buttons id
   const handleClick = (e) => {
     e.preventDefault();
     setter(e.target.id);
   };
 
+  // Must create an array to push each button into.
+  // Without pushing into array, the for loop exits
+  // after one cycle when the first return is hit.
   const RenderButtons = () => {
     const array = [];
 
