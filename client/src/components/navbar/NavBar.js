41391f49753ebe121c1acb29f1af49b425a8cbff
NicMortelliti <npmortelliti@gmail.com>
2022-11-17 16:18:17 -0800

    Reorganize nav bar

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -2,22 +2,45 @@ import React from "react";
 import Logout from "../account/Logout";
 
 function NavBar({ user, setUser, setDisplayedPage }) {
+  // Display public and user facing button
+  const handleClick = (e) => {
+    e.preventDefault();
+    setDisplayedPage(e.target.name);
+  };
+
+  // Display nav bar background
+  const RenderNavBarBackground = () => (
+    <div
+      style={{
+        backgroundColor: "#1b1b1d",
+        width: "100vw",
+        height: "2em",
+        border: "1px solid #1b1b1d",
+      }}>
+      <RenderNavBarButtons />
+    </div>
+  );
+
+  // Display nav bar buttons
+  const RenderNavBarButtons = () =>
+    user ? (
+      <div>
+        <button name="main" onClick={(e) => handleClick(e)}>
+          Home
+        </button>
+        <button name="search" onClick={(e) => handleClick(e)}>
+          Book
+        </button>
+        <button onClick={() => setDisplayedPage("reservations")}>
+          My Trips
+        </button>
+        <Logout setUser={setUser} setDisplayedPage={setDisplayedPage} />
+      </div>
+    ) : null;
+
   return (
     <div>
-      <button onClick={() => setDisplayedPage("main")}>Home</button>
-      <button onClick={() => setDisplayedPage("search")}>Book</button>
-      {user ? (
-        <>
-          <button onClick={() => setDisplayedPage("checkin")}>Check-In</button>
-          <button onClick={() => setDisplayedPage("reservations")}>
-            My Trips
-          </button>
-        </>
-      ) : null}
-      <button onClick={() => setDisplayedPage("status")}>Flight Status</button>
-      {user ? (
-        <Logout setUser={setUser} setDisplayedPage={setDisplayedPage} />
-      ) : null}
+      <RenderNavBarBackground />
     </div>
   );
 }
