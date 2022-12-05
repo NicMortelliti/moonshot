506debc6705efd882f8a6e851f84a2234448987e
Nic Mortelliti <npmortelliti@gmail.com>
2022-12-04 18:45:36 -0800

    Add new profile branch

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProfileForm from "./ProfileForm";
+
+const Profile = () => {
+  return (
+    <div>
+      <ProfileForm />
+    </div>
+  );
+};
+
+export default Profile;
