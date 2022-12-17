5818561ece22c3e9156ef9c9e938aa5d6e4261ce
Nic Mortelliti <npmortelliti@gmail.com>
2022-12-17 14:20:18 -0800

    Add basic content to about section

--- client/src/components/landing/About.js
+++ client/src/components/landing/About.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+      }}>
+      <h2>The final frontier is just the beginning...</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, libero.
+        Corporis nostrum dolores saepe sequi culpa? Tenetur consectetur
+        blanditiis sunt corporis nam cupiditate eaque ipsam voluptas assumenda
+        repudiandae. Distinctio, quidem?
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
