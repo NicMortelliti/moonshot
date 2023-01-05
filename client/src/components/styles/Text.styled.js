ea8cd5d31cad7d044c9f43bb1ff6453ed3e195f6
Nic Mortelliti <npmortelliti@gmail.com>
2023-01-04 18:40:54 -0800

    Make text more legible

--- client/src/components/styles/Text.styled.js
+++ client/src/components/styles/Text.styled.js
@@ -8,7 +8,7 @@ const color = ({ light }) => (light ? theme.light : "black");
 export const H1 = styled.h1`
   font-family: ${({ fancy }) => fancy && fancyFont};
   text-shadow: ${({ theme, fancy }) =>
-    fancy ? `${theme.accent} 1px 0 10px` : null};
+    fancy ? `${theme.accent} 1px 0 10px, black 2px 2px 5px` : null};
   font-size: clamp(1rem, -0.875rem + 8.333333vw, 5rem);
   color: ${color};
 `;
@@ -17,6 +17,7 @@ export const H2 = styled.h2`
   font-family: ${({ fancy }) => fancy && fancyFont};
   align-self: ${({ center }) => center && "center"};
   color: ${color};
+  text-shadow: ${({ theme, fancy }) => (fancy ? `black 2px 2px 1px` : null)};
 `;
 
 export const H3 = styled.h3`
@@ -37,4 +38,5 @@ export const H5 = styled.h5`
 export const P = styled.p`
   color: ${color};
   font-weight: ${({ fancy }) => fancy && "lighter"};
+  text-shadow: ${({ theme, fancy }) => (fancy ? ` black 2px 2px 1px` : null)};
 `;
