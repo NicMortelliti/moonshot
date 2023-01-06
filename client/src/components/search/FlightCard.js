653155e370017e6e859ad159a028e689075a47e6
Nic Mortelliti <npmortelliti@gmail.com>
2023-01-06 15:11:54 -0800

    Remove dead import

--- client/src/components/search/FlightCard.js
+++ client/src/components/search/FlightCard.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { bookFlight } from "../../features/booking/bookingSlice";
-import ActionConfirmation from "../card/components/ActionConfirmation";
 import { formatDate } from "../../helpers/helpers";
 import {
   FlightGrid,
