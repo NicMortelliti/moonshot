e4445f751b88919c12dff6c350c5beac1262a609
Nic Mortelliti <npmortelliti@gmail.com>
2023-01-09 10:21:17 -0800

    Update about

--- client/src/components/landing/About.js
+++ client/src/components/landing/About.js
@@ -14,7 +14,7 @@ const About = () => {
         The final frontier is just the beginning...
       </H2>
       <P light fancy>
-        Space tourism is now more accessible than ever before. Our company offers regular flights to the cosmos, giving you the opportunity to experience the thrill of weightlessness and witness the beauty of Earth from above. Our state-of-the-art spacecraft and highly trained pilots ensure a safe and comfortable journey beyond the atmosphere. Imagine the awe-inspiring views of the universe and the chance to be one of the few people in history to travel to space. Don't miss this opportunity to explore the final frontier. Book your flight today and join us on a journey of a lifetime.
+        Space tourism is now more accessible than ever before. Moonshot offers regular flights to the cosmos, giving you the opportunity to experience the thrill of weightlessness and witness the beauty of our solar system firsthand. Our state-of-the-art spacecraft and highly trained pilots ensure a safe and comfortable journey beyond the atmosphere. Imagine the awe-inspiring views of the universe and the chance to be one of the few people in history to travel to space. Don't miss this opportunity to explore the final frontier. Book your flight today and join us on a journey of a lifetime.
       </P>
     </Container>
   );
