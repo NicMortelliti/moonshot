ee7e679b38c3943043222c3dd6e3b209e3ad3dbe
Nic Mortelliti <npmortelliti@gmail.com>
2023-01-10 09:50:38 -0800

    Delete dead code

--- client/src/components/reservations/ReservationCard.js
+++ client/src/components/reservations/ReservationCard.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { deleteReservation } from "../../features/reservations/reservationSlice";
 import { formatDate } from "../../helpers/helpers";
 import {
